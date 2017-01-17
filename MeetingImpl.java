679cf610c26addf092eaf7ea025c512fd23d72e8
eserra02 <enricserrasanz@gmail.com>
2017-01-17 19:25:51 +0000

    all Meeting tests pass

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -1,53 +1,39 @@
 import java.util.Calendar;
 import java.util.Set;
 import java.util.*;
+
 /**
  * A class to represent meetings
  *
  * Meetings have unique IDs, scheduled date and a list of participating contacts
  */
+
 public class MeetingImpl implements Meeting {
     private int meetingId;
+    private Calendar meetingDate;
+    private Set<Contact> contactsSet;
+
     public static int totalMeetings = 0;
 
-    /**
-     * Returns the id of the meeting.
-     *
-     * @return the id of the meeting.
-     */
-    public MeetingImpl() {
+
+    public MeetingImpl(Calendar date, Set<Contact> contactsSet) {
         totalMeetings++;
         this.meetingId = totalMeetings;
+        this.meetingDate = date;
+        this.contactsSet = contactsSet;
     }
 
     public int getId() {
         return meetingId;
     }
 
-    /**
-     * Return the date of the meeting.
-     *
-     * @return the date of the meeting.
-     */
     public Calendar getDate() {
-        Calendar cal = Calendar.getInstance();
-        return cal;
+        return meetingDate;
     }
 
-    /**
-     * Return the details of people that attended the meeting.
-     *
-     * The list contains a minimum of one contact (if there were
-     * just two people: the user and the contact) and may contain an
-     * arbitrary number of them.
-     *
-     * @return the details of people that attended the meeting.
-     */
     public Set<Contact> getContacts() {
-        Set<Integer> mySet = new HashSet<>();
-
-        Set<Contact> contacts = new HashSet<>();
-        return contacts;
+        return this.contactsSet;
 
     }
+
 }
