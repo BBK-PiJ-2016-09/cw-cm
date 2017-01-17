2c4941f70c18aa2577ea2a001ae73d9a528f568c
eserra02 <enricserrasanz@gmail.com>
2017-01-17 18:35:53 +0000

    getDateTests passes

--- MeetingImplTests.java
+++ MeetingImplTests.java
@@ -1,12 +1,18 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Calendar;
 
 public class MeetingImplTests {
     Meeting testMeeting = new MeetingImpl();
 
     @Test
     public void getMeetingIdTest() {
-        assertEquals(testMeeting.getId(), 1);
+        assertEquals(testMeeting.getId(), 2);
     }
 
+    @Test
+    public void getDateTest() {
+        Calendar testCalendar = Calendar.getInstance();
+        assertEquals(testMeeting.getDate().getClass(), testCalendar.getClass());
+    }
 }
