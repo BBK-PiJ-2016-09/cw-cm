74b83cd6a7b5e123017f9eb35399fbec8e917293
eserra02 <enricserrasanz@gmail.com>
2017-01-17 19:06:25 +0000

    getContacts test fails

--- MeetingImplTests.java
+++ MeetingImplTests.java
@@ -1,18 +1,30 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Calendar;
+import java.util.Set;
+import java.util.*;
 
 public class MeetingImplTests {
-    Meeting testMeeting = new MeetingImpl();
+
+    Calendar myDate = Calendar.getInstance();
+    Meeting testMeeting = new MeetingImpl(myDate);
+    Contact testContact = new ContactImpl("contactName");
+    Set<Contact> testContactSet = new HashSet<>();
+
+
 
     @Test
     public void getMeetingIdTest() {
-        assertEquals(testMeeting.getId(), 2);
+        assertEquals(testMeeting.getId(), 3);
     }
 
     @Test
     public void getDateTest() {
-        Calendar testCalendar = Calendar.getInstance();
-        assertEquals(testMeeting.getDate().getClass(), testCalendar.getClass());
+        assertEquals(testMeeting.getDate(), myDate);
+    }
+    @Test
+    public void getContactsTest() {
+        testContactSet.add(testContact);
+        assertEquals(testMeeting.getContacts(), testContactSet);
     }
 }
