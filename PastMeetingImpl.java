6e6dd8373c0f6293fe6674fd7f1fc07ab21fa66f
eserra02 <enricserrasanz@gmail.com>
2017-01-30 10:38:11 +0000

    prepared PastMeeting constructor

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -1,6 +1,10 @@
+import java.util.Calendar;
+import java.util.Set;
+import java.util.*;
+
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
-    public PastMEetingImpl(){
-        return super;
+    public PastMeetingImpl(Calendar date, Set<Contact> contactsSet){
+        super(date, contactsSet);
     }
     public String getNotes() {
         return "";
